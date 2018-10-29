14138f80c2a189b86aa399c50e99794f5010c552
ARYPHAMM <quoctienphamm@gmail.com>
2018-10-29 10:21:47 +0700

    frm Dang nhap va frm Quan ly

---
+++ QLKaraoke/FrmDangNhapcs.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace QLKaraoke
+{
+    partial class FrmDangNhapcs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMK = new System.Windows.Forms.TextBox();
+            this.txtTK = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnDangNhap);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtMK);
+            this.panel1.Controls.Add(this.txtTK);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(61, 77);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(703, 402);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangNhap.Location = new System.Drawing.Point(257, 299);
+            this.btnDangNhap.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(299, 50);
+            this.btnDangNhap.TabIndex = 5;
+            this.btnDangNhap.Text = "ĐĂNG NHẬP";
+            this.btnDangNhap.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(141, 249);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Mật khẩu:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(95, 196);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tên đăng nhập:";
+            // 
+            // txtMK
+            // 
+            this.txtMK.Location = new System.Drawing.Point(257, 249);
+            this.txtMK.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMK.Name = "txtMK";
+            this.txtMK.Size = new System.Drawing.Size(297, 22);
+            this.txtMK.TabIndex = 2;
+            this.txtMK.UseSystemPasswordChar = true;
+            // 
+            // txtTK
+            // 
+            this.txtTK.Location = new System.Drawing.Point(257, 196);
+            this.txtTK.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTK.Name = "txtTK";
+            this.txtTK.Size = new System.Drawing.Size(297, 22);
+            this.txtTK.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SpringGreen;
+            this.label1.Location = new System.Drawing.Point(224, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 92);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phần Mền Quản Lý\r\n   Phòng Karaoke";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmDangNhapcs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 557);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmDangNhapcs";
+            this.Text = "FrmDangNhapcs";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMK;
+        private System.Windows.Forms.TextBox txtTK;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ QLKaraoke/FrmQuanLyKaraoke.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace QLKaraoke
+{
+    partial class FrmQuanLyKaraoke
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.mniHoaDon = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniQuanLy = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniSua = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniLoaiSua = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniNhanVien = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniBaoCao = new System.Windows.Forms.ToolStripMenuItem();
+            this.lậpHóaĐơnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cậpNhậtHóaĐơnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tàiKhoảnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thôngKêToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.mnuMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuMain
+            // 
+            this.mnuMain.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mnuMain.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mnuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniHoaDon,
+            this.mniQuanLy,
+            this.mniBaoCao,
+            this.thôngKêToolStripMenuItem});
+            this.mnuMain.Location = new System.Drawing.Point(0, 0);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.mnuMain.Size = new System.Drawing.Size(914, 36);
+            this.mnuMain.TabIndex = 1;
+            this.mnuMain.Text = "menuStrip1";
+            // 
+            // mniHoaDon
+            // 
+            this.mniHoaDon.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lậpHóaĐơnToolStripMenuItem,
+            this.cậpNhậtHóaĐơnToolStripMenuItem});
+            this.mniHoaDon.Name = "mniHoaDon";
+            this.mniHoaDon.Size = new System.Drawing.Size(108, 32);
+            this.mniHoaDon.Text = "Hóa Đơn";
+            // 
+            // mniQuanLy
+            // 
+            this.mniQuanLy.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniSua,
+            this.mniLoaiSua,
+            this.mniNhanVien,
+            this.tàiKhoảnToolStripMenuItem});
+            this.mniQuanLy.Name = "mniQuanLy";
+            this.mniQuanLy.Size = new System.Drawing.Size(100, 32);
+            this.mniQuanLy.Text = "Quản Lý";
+            // 
+            // mniSua
+            // 
+            this.mniSua.Name = "mniSua";
+            this.mniSua.Size = new System.Drawing.Size(189, 32);
+            this.mniSua.Text = "Phòng";
+            // 
+            // mniLoaiSua
+            // 
+            this.mniLoaiSua.Name = "mniLoaiSua";
+            this.mniLoaiSua.Size = new System.Drawing.Size(189, 32);
+            this.mniLoaiSua.Text = "Thực Đơn";
+            // 
+            // mniNhanVien
+            // 
+            this.mniNhanVien.Name = "mniNhanVien";
+            this.mniNhanVien.Size = new System.Drawing.Size(189, 32);
+            this.mniNhanVien.Text = "Nhân Viên";
+            // 
+            // mniBaoCao
+            // 
+            this.mniBaoCao.Name = "mniBaoCao";
+            this.mniBaoCao.Size = new System.Drawing.Size(12, 32);
+            // 
+            // lậpHóaĐơnToolStripMenuItem
+            // 
+            this.lậpHóaĐơnToolStripMenuItem.Name = "lậpHóaĐơnToolStripMenuItem";
+            this.lậpHóaĐơnToolStripMenuItem.Size = new System.Drawing.Size(259, 32);
+            this.lậpHóaĐơnToolStripMenuItem.Text = "Lập Hóa Đơn";
+            // 
+            // cậpNhậtHóaĐơnToolStripMenuItem
+            // 
+            this.cậpNhậtHóaĐơnToolStripMenuItem.Name = "cậpNhậtHóaĐơnToolStripMenuItem";
+            this.cậpNhậtHóaĐơnToolStripMenuItem.Size = new System.Drawing.Size(259, 32);
+            this.cậpNhậtHóaĐơnToolStripMenuItem.Text = "Cập nhật hóa đơn";
+            // 
+            // tàiKhoảnToolStripMenuItem
+            // 
+            this.tàiKhoảnToolStripMenuItem.Name = "tàiKhoảnToolStripMenuItem";
+            this.tàiKhoảnToolStripMenuItem.Size = new System.Drawing.Size(189, 32);
+            this.tàiKhoảnToolStripMenuItem.Text = "Tài Khoản";
+            // 
+            // thôngKêToolStripMenuItem
+            // 
+            this.thôngKêToolStripMenuItem.Name = "thôngKêToolStripMenuItem";
+            this.thôngKêToolStripMenuItem.Size = new System.Drawing.Size(114, 32);
+            this.thôngKêToolStripMenuItem.Text = "Thông Kê";
+            this.thôngKêToolStripMenuItem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDangNhap.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnDangNhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDangNhap.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnDangNhap.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDangNhap.Location = new System.Drawing.Point(787, 3);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(115, 33);
+            this.btnDangNhap.TabIndex = 0;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = false;
+            // 
+            // FrmQuanLyKaraoke
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 593);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.mnuMain);
+            this.IsMdiContainer = true;
+            this.Name = "FrmQuanLyKaraoke";
+            this.Text = "Quản Lý Phòng Karaoke";
+            this.mnuMain.ResumeLayout(false);
+            this.mnuMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuMain;
+        private System.Windows.Forms.ToolStripMenuItem mniHoaDon;
+        private System.Windows.Forms.ToolStripMenuItem mniQuanLy;
+        private System.Windows.Forms.ToolStripMenuItem mniSua;
+        private System.Windows.Forms.ToolStripMenuItem mniLoaiSua;
+        private System.Windows.Forms.ToolStripMenuItem mniNhanVien;
+        private System.Windows.Forms.ToolStripMenuItem mniBaoCao;
+        private System.Windows.Forms.ToolStripMenuItem lậpHóaĐơnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cậpNhậtHóaĐơnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tàiKhoảnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thôngKêToolStripMenuItem;
+        private System.Windows.Forms.Button btnDangNhap;
+    }
+}
