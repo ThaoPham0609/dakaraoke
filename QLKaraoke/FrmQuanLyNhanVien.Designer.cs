294c1cd3c5a8a4a78539719262cc029765107f88
ARYPHAMM <quoctienphamm@gmail.com>
2018-10-29 16:01:40 +0700

    up cac from quan ly

---
+++ QLKaraoke/FrmQuanLyNhanVien.Designer.cs
@@ -0,0 +1,415 @@
+﻿namespace QLKaraoke
+{
+    partial class FrmQuanLyNhanVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.btnquayve = new System.Windows.Forms.Button();
+            this.dtpnamsinh = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btntracuunv = new System.Windows.Forms.Button();
+            this.btncapnhatnv = new System.Windows.Forms.Button();
+            this.btnthemnv = new System.Windows.Forms.Button();
+            this.colTINHTRANG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDIACHI = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEMAIL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSDTNV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colLOAINV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNAMSINH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTENV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colHONV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colMANV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvquanlynv = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvquanlynv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(886, 87);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 22;
+            // 
+            // btnquayve
+            // 
+            this.btnquayve.Location = new System.Drawing.Point(1131, 21);
+            this.btnquayve.Name = "btnquayve";
+            this.btnquayve.Size = new System.Drawing.Size(121, 35);
+            this.btnquayve.TabIndex = 21;
+            this.btnquayve.Text = "Quay Về";
+            this.btnquayve.UseVisualStyleBackColor = true;
+            // 
+            // dtpnamsinh
+            // 
+            this.dtpnamsinh.Location = new System.Drawing.Point(540, 34);
+            this.dtpnamsinh.Name = "dtpnamsinh";
+            this.dtpnamsinh.Size = new System.Drawing.Size(200, 22);
+            this.dtpnamsinh.TabIndex = 20;
+            this.dtpnamsinh.Value = new System.DateTime(2018, 12, 8, 0, 0, 0, 0);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Location = new System.Drawing.Point(540, 87);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(243, 141);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 22);
+            this.textBox3.TabIndex = 18;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(243, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 22);
+            this.textBox2.TabIndex = 18;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(540, 141);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(213, 22);
+            this.textBox4.TabIndex = 18;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(886, 141);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(213, 22);
+            this.textBox7.TabIndex = 18;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(886, 34);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(213, 22);
+            this.textBox5.TabIndex = 18;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(243, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 22);
+            this.textBox1.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(791, 92);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(78, 17);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "TÌnh Trạng";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(791, 146);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 17);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Địa Chỉ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(791, 39);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 17);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Email";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(430, 146);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Số Điện Thoại";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(430, 92);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Loại NV";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(430, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Năm Sinh";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(167, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Tên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(167, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Họ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "MANV";
+            // 
+            // btntracuunv
+            // 
+            this.btntracuunv.Location = new System.Drawing.Point(6, 126);
+            this.btntracuunv.Name = "btntracuunv";
+            this.btntracuunv.Size = new System.Drawing.Size(121, 37);
+            this.btntracuunv.TabIndex = 8;
+            this.btntracuunv.Text = "Tra cứu";
+            this.btntracuunv.UseVisualStyleBackColor = true;
+            // 
+            // btncapnhatnv
+            // 
+            this.btncapnhatnv.Location = new System.Drawing.Point(7, 72);
+            this.btncapnhatnv.Name = "btncapnhatnv";
+            this.btncapnhatnv.Size = new System.Drawing.Size(120, 37);
+            this.btncapnhatnv.TabIndex = 7;
+            this.btncapnhatnv.Text = "Cập nhật";
+            this.btncapnhatnv.UseVisualStyleBackColor = true;
+            // 
+            // btnthemnv
+            // 
+            this.btnthemnv.Location = new System.Drawing.Point(6, 21);
+            this.btnthemnv.Name = "btnthemnv";
+            this.btnthemnv.Size = new System.Drawing.Size(121, 35);
+            this.btnthemnv.TabIndex = 6;
+            this.btnthemnv.Text = "Thêm";
+            this.btnthemnv.UseVisualStyleBackColor = true;
+            // 
+            // colTINHTRANG
+            // 
+            this.colTINHTRANG.HeaderText = "Tình Trạng";
+            this.colTINHTRANG.Name = "colTINHTRANG";
+            // 
+            // colDIACHI
+            // 
+            this.colDIACHI.HeaderText = "Địa Chỉ";
+            this.colDIACHI.Name = "colDIACHI";
+            // 
+            // colEMAIL
+            // 
+            this.colEMAIL.HeaderText = "Email";
+            this.colEMAIL.Name = "colEMAIL";
+            // 
+            // colSDTNV
+            // 
+            this.colSDTNV.HeaderText = "Số Điện Thoại";
+            this.colSDTNV.Name = "colSDTNV";
+            // 
+            // colLOAINV
+            // 
+            this.colLOAINV.HeaderText = "Loại NV";
+            this.colLOAINV.Name = "colLOAINV";
+            // 
+            // colNAMSINH
+            // 
+            this.colNAMSINH.HeaderText = "Năm Sinh";
+            this.colNAMSINH.Name = "colNAMSINH";
+            // 
+            // colTENV
+            // 
+            this.colTENV.HeaderText = "Tên";
+            this.colTENV.Name = "colTENV";
+            // 
+            // colHONV
+            // 
+            this.colHONV.HeaderText = "Họ";
+            this.colHONV.Name = "colHONV";
+            // 
+            // colMANV
+            // 
+            this.colMANV.HeaderText = "MANV";
+            this.colMANV.Name = "colMANV";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.btnquayve);
+            this.groupBox1.Controls.Add(this.dtpnamsinh);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox7);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btntracuunv);
+            this.groupBox1.Controls.Add(this.btncapnhatnv);
+            this.groupBox1.Controls.Add(this.btnthemnv);
+            this.groupBox1.Location = new System.Drawing.Point(11, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1116, 174);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin";
+            // 
+            // dgvquanlynv
+            // 
+            this.dgvquanlynv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvquanlynv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvquanlynv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvquanlynv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colMANV,
+            this.colHONV,
+            this.colTENV,
+            this.colNAMSINH,
+            this.colLOAINV,
+            this.colSDTNV,
+            this.colEMAIL,
+            this.colDIACHI,
+            this.colTINHTRANG});
+            this.dgvquanlynv.Location = new System.Drawing.Point(12, 242);
+            this.dgvquanlynv.Name = "dgvquanlynv";
+            this.dgvquanlynv.RowTemplate.Height = 24;
+            this.dgvquanlynv.Size = new System.Drawing.Size(1115, 381);
+            this.dgvquanlynv.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(190, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(777, 34);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Quản Lý Nhân Viên";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmQuanLyNhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1139, 646);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvquanlynv);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmQuanLyNhanVien";
+            this.Text = "FrmQuanLyNhanVien";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvquanlynv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button btnquayve;
+        private System.Windows.Forms.DateTimePicker dtpnamsinh;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btntracuunv;
+        private System.Windows.Forms.Button btncapnhatnv;
+        private System.Windows.Forms.Button btnthemnv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTINHTRANG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDIACHI;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEMAIL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSDTNV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLOAINV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNAMSINH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTENV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colHONV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMANV;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvquanlynv;
+        private System.Windows.Forms.Label label1;
+    }
+}
